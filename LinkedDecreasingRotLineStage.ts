7fe37f2169b2033b6bcc62fd23287f4e4257a4d1
Anwesh43 <anweshthecool0@gmail.com>
2018-07-08 15:27:42 +0530

    adding handletap and canvas in document body

---
+++ LinkedDecreasingRotLineStage.ts
@@ -0,0 +1,36 @@
+const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+class LinkedDecreasingRotLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+            alert('clicked on canvas')
+        }
+    }
+
+    static init() {
+        const stage = new LinkedDecreasingRotLineStage()
+        stage.render()
+        stage.handleTap()
+    }
+
+}
